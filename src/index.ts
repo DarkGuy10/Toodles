f1ed0ae13935fa66f1071aa53d5acee444e5015c
DarkGuy10 <62807269+DarkGuy10@users.noreply.github.com>
2023-12-12 14:13:10 +0530

    feat: hardcode ANSII color strings

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,6 @@
-import chalk from "chalk";
-
 type LogLevel = "ERROR" | "SUCCESS" | "INFO" | "PROGRESS" | "WARN";
 
 class Toodles {
-	/**
-	 * Colors for different log levels
-	 * Based on the Catpuccin Mocha color pallete: https://github.com/catppuccin/catppuccin
-	 */
-	private _colorLevels = {
-		INFO: chalk.hex("#89b4fa"),
-		ERROR: chalk.hex("#f38ba8"),
-		SUCCESS: chalk.hex("#a6e3a1"),
-		PROGRESS: chalk.hex("#cba6f7"),
-		WARN: chalk.hex("#f9e2af"),
-	};
-
-	/**
-	 * Returns text blocks prepended to different types of messages
-	 * @param logLevel Log level of the prefix
-	 */
-	prefix(logLevel: LogLevel): string {
-		return `${chalk.grey("[")} ${this._colorLevels[logLevel].bold(
-			logLevel
-		)} ${chalk.grey("]")}`;
-	}
-
 	/**
 	 * Print a plain message (same as console.log)
 	 * @param message Message to print
@@ -38,7 +14,7 @@ class Toodles {
 	 * @param message Message to print
 	 */
 	error(message: unknown): void {
-		console.log(`${this.prefix("ERROR")} ${this._colorLevels.ERROR(message)}`);
+		console.log(`[90m[[39m [38;2;243;139;168m[1mERROR[22m[39m [90m][39m [38;2;243;139;168m${message}[39m`);
 	}
 
 	/**
@@ -46,9 +22,7 @@ class Toodles {
 	 * @param message Message to print
 	 */
 	success(message: unknown): void {
-		console.log(
-			`${this.prefix("SUCCESS")} ${this._colorLevels.SUCCESS(message)}`
-		);
+		console.log(`[90m[[39m [38;2;166;227;161m[1mSUCCESS[22m[39m [90m][39m [38;2;166;227;161m${message}[39m`);
 	}
 
 	/**
@@ -56,7 +30,7 @@ class Toodles {
 	 * @param message Message to print
 	 */
 	info(message: unknown): void {
-		console.log(`${this.prefix("INFO")} ${this._colorLevels.INFO(message)}`);
+		console.log(`[90m[[39m [38;2;163;185;239m[1mINFO[22m[39m [90m][39m [38;2;163;185;239m${message}[39m`);
 	}
 
 	/**
@@ -64,7 +38,7 @@ class Toodles {
 	 * @param message Message to print
 	 */
 	warn(message: unknown): void {
-		console.log(`${this.prefix("WARN")} ${this._colorLevels.WARN(message)}`);
+		console.log(`[90m[[39m [38;2;249;226;175m[1mWARN[22m[39m [90m][39m [38;2;249;226;175m${message}[39m`);
 	}
 
 	/**
@@ -72,9 +46,7 @@ class Toodles {
 	 * @param message Message to print
 	 */
 	progress(message: unknown): void {
-		console.log(
-			`${this.prefix("PROGRESS")} ${this._colorLevels.PROGRESS(message)}`
-		);
+		console.log(`[90m[[39m [38;2;203;166;247m[1mPROGRESS[22m[39m [90m][39m [38;2;203;166;247m${message}[39m`);
 	}
 }
 
