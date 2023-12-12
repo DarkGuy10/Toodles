d94bf5026ca796a3284b4a4dc4c1cfc506e840ff
DarkGuy10 <62807269+DarkGuy10@users.noreply.github.com>
2023-12-12 14:18:44 +0530

    chore: add `test.ts`

---
+++ src/test.ts
@@ -0,0 +1,8 @@
+import Toodles from "./index.js";
+
+Toodles.plain("Plain");
+Toodles.error("Error");
+Toodles.info("Info");
+Toodles.progress("Progress");
+Toodles.success("Success");
+Toodles.warn("Warn");
